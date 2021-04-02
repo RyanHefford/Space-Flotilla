028cb3b4f5c7d546109cecb5e493e2c96b089df9
nathanielgecse <39307971+nathanielgecse@users.noreply.github.com>
2021-04-02 19:45:31 +1100

    added overlord prefab, flock still not following overlord

--- Assets/Behaviour Scripts/StickToOverlordBehaviour.cs
+++ Assets/Behaviour Scripts/StickToOverlordBehaviour.cs
@@ -7,20 +7,21 @@ public class StickToOverlordBehaviour : FlockBehaviour
 {
     private GameObject overlord;
     private Rigidbody2D rigidbody;
-    private Vector2 overlord_centre;
+    private Vector2 overlordCentre;
     public float overlordRadius = 2.0f;
 
     public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
     {
-        overlord = GameObject.Find("Player");
+        overlord = GameObject.Find("Overlord");
         rigidbody = overlord.GetComponent<Rigidbody2D>();
-        overlord_centre = rigidbody.centerOfMass;
+        overlordCentre = rigidbody.centerOfMass;
 
-        Vector2 overlordOffset = overlord_centre - (Vector2)agent.transform.position;
+        Vector2 overlordOffset = overlordCentre - (Vector2)agent.transform.position;
         float t = overlordOffset.magnitude / overlordRadius;
         if (t < 0.9f)
         {
             return Vector2.zero;
+            // return overlordCentre;
         }
 
         return overlordOffset * t * t;
