46123d9a976c9430f9ea2df68751ee8704a23556
nathanielgecse <39307971+nathanielgecse@users.noreply.github.com>
2021-04-05 13:19:59 +1000

    fixed flocking to overlord object

--- Assets/Behaviour Scripts/StickToOverlordBehaviour.cs
+++ Assets/Behaviour Scripts/StickToOverlordBehaviour.cs
@@ -12,16 +12,14 @@ public class StickToOverlordBehaviour : FlockBehaviour
 
     public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
     {
-        overlord = GameObject.Find("Overlord");
-        rigidbody = overlord.GetComponent<Rigidbody2D>();
-        overlordCentre = rigidbody.centerOfMass;
+        overlord = GameObject.FindGameObjectsWithTag("Overlord")[0];
+        overlordCentre = overlord.transform.position;
 
         Vector2 overlordOffset = overlordCentre - (Vector2)agent.transform.position;
         float t = overlordOffset.magnitude / overlordRadius;
         if (t < 0.9f)
         {
-            return Vector2.zero;
-            // return overlordCentre;
+            return overlordCentre;
         }
 
         return overlordOffset * t * t;
