909e6df4c42e551e859984fd4acd215186a158db
Talal Meshari <s3628451@student.rmit.edu.au>
2021-04-17 04:38:15 +1000

    Added some code for path smoothing. Will hopefully get it done tomorrow

--- Assets/Scripts/Pathfinding/EnemyAI.cs
+++ Assets/Scripts/Pathfinding/EnemyAI.cs
@@ -34,7 +34,8 @@ public class EnemyAI : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         //update the path every 0.5f seconds
-        InvokeRepeating("updatePath", 0f, 0.5f);
+        //InvokeRepeating("updatePath", 0f, 0.5f);
+        InvokeRepeating("updatePath", 0f, 5f);
 
     }
 
@@ -51,7 +52,7 @@ public class EnemyAI : MonoBehaviour
         }
         
     }
-
+    private List<Vector2> testV = new List<Vector2>();
     void onPathCompleted(Path p)
     {
         //make sure no error
@@ -60,7 +61,9 @@ public class EnemyAI : MonoBehaviour
             //path smoothing.
             //p.vectorPath = pathSmoothing(p);
 
-            
+            testV = pathSmoothing(p);
+
+
 
             //current path = the newly generated path
             path = p;
@@ -68,18 +71,88 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private List<Vector3> pathSmoothing(Path p)
+    
+
+    void OnDrawGizmos()
     {
-        Vector3 currentPosition = rb.transform.position;
-        List<Vector3> newVectorPath = new List<Vector3>();
 
-        foreach (Vector3 nextWaypoint in p.vectorPath)
+        // Draws a blue line from this transform to the target
+
+        for (int i = 0; i < testV.Count; i += 2)
         {
+            Gizmos.color = Color.cyan;
+            print(i);
 
+            if(testV.Count % 2 != 0)
+            {
+                Gizmos.DrawLine(testV[i], testV[i + 1]);
+            }
+            else
+            {
+                if(i == testV.Count)
+                {
+                    Gizmos.DrawLine(testV[i-2], testV[i - 1]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(testV[i], testV[i + 1]);
+                }
+            }
             
+
+        }
+
+        //for (int i = 0; i < path.vectorPath.Count; i += 2)
+        //{
+        //    Gizmos.color = Color.gray;
+        //    Gizmos.DrawLine(path.vectorPath[i], path.vectorPath[i + 1]);
+
+        //}
+
+    }
+
+    private List<Vector2> pathSmoothing(Path p)
+    {
+        Vector2 currentPosition = rb.transform.position;
+        List<Vector2> newVectorPath = new List<Vector2>();
+        Vector2 previousPosition = Vector2.zero;
+
+        //adding the start.
+        newVectorPath.Add(currentPosition);
+        previousPosition = currentPosition;
+        int count = 0;
+        print("Vector path count: " + p.vectorPath.Count);
+        foreach (Vector2 nextWaypoint in p.vectorPath)
+        {
+            if(previousPosition == nextWaypoint && count == 0)
+            {
+                count = 1;
+                continue;
+                
+            }
+            float distance = Vector2.Distance(currentPosition, target.position);
+            print(distance);
+            //Debug.DrawRay(currentPosition, ((Vector2)target.position - currentPosition) * 10, Color.red);
+            RaycastHit2D hit = Physics2D.Raycast(currentPosition, ((Vector2)target.position - currentPosition), distance, 1 << LayerMask.NameToLayer("Obstacle"));
+
+            if (hit)
+            {
+                //we hit something:
+                print("HIT HIT HIT");
+                print(hit.transform.gameObject.name);
+                newVectorPath.Add(previousPosition);
+                currentPosition = previousPosition;
+
+            }
+            previousPosition = nextWaypoint;
+
 
 
         }
+        // add the target at the end.
+        newVectorPath.Add(target.position);
+
+        print(newVectorPath.Count);
 
         return newVectorPath;
     }
