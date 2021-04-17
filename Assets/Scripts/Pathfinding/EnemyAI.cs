53a36e1f259cec5f281b1e994533f16d6eade63b
Talal Meshari <s3628451@student.rmit.edu.au>
2021-04-17 14:31:00 +1000

    Finished path smoothing

--- Assets/Scripts/Pathfinding/EnemyAI.cs
+++ Assets/Scripts/Pathfinding/EnemyAI.cs
@@ -52,7 +52,7 @@ public class EnemyAI : MonoBehaviour
         }
         
     }
-    private List<Vector2> testV = new List<Vector2>();
+    private List<Vector3> testV = new List<Vector3>();
     void onPathCompleted(Path p)
     {
         //make sure no error
@@ -67,6 +67,7 @@ public class EnemyAI : MonoBehaviour
 
             //current path = the newly generated path
             path = p;
+            p.vectorPath = testV;
             currentWaypoint = 0;
         }
     }
@@ -81,17 +82,16 @@ public class EnemyAI : MonoBehaviour
         for (int i = 0; i < testV.Count; i += 2)
         {
             Gizmos.color = Color.cyan;
-            print(i);
 
-            if(testV.Count % 2 != 0)
+            if(testV.Count % 2 == 0)
             {
                 Gizmos.DrawLine(testV[i], testV[i + 1]);
             }
             else
             {
-                if(i == testV.Count)
+                if(i+1 == testV.Count)
                 {
-                    Gizmos.DrawLine(testV[i-2], testV[i - 1]);
+                    Gizmos.DrawLine(testV[i-1], testV[i]);
                 }
                 else
                 {
@@ -111,29 +111,58 @@ public class EnemyAI : MonoBehaviour
 
     }
 
-    private List<Vector2> pathSmoothing(Path p)
+    private List<Vector3> pathSmoothing(Path p)
     {
-        Vector2 currentPosition = rb.transform.position;
-        List<Vector2> newVectorPath = new List<Vector2>();
-        Vector2 previousPosition = Vector2.zero;
+        Vector3 currentPosition = rb.transform.position;
+        List<Vector3> newVectorPath = new List<Vector3>();
+        Vector3 previousPosition = Vector3.zero;
 
         //adding the start.
         newVectorPath.Add(currentPosition);
         previousPosition = currentPosition;
         int count = 0;
-        print("Vector path count: " + p.vectorPath.Count);
-        foreach (Vector2 nextWaypoint in p.vectorPath)
-        {
-            if(previousPosition == nextWaypoint && count == 0)
-            {
-                count = 1;
-                continue;
+        //print("Vector path count: " + p.vectorPath.Count);
+        //foreach (Vector2 nextWaypoint in p.vectorPath)
+        //{
+        //    //if(previousPosition == nextWaypoint && count == 0)
+        //    //{
+        //    //    count = 1;
+        //    //    continue;
                 
-            }
-            float distance = Vector2.Distance(currentPosition, target.position);
-            print(distance);
-            //Debug.DrawRay(currentPosition, ((Vector2)target.position - currentPosition) * 10, Color.red);
-            RaycastHit2D hit = Physics2D.Raycast(currentPosition, ((Vector2)target.position - currentPosition), distance, 1 << LayerMask.NameToLayer("Obstacle"));
+        //    //}
+
+        //    float distance = Vector2.Distance(currentPosition, nextWaypoint);
+
+        //    Vector2 to = ((Vector2)nextWaypoint - currentPosition);
+
+        //    Debug.DrawRay(currentPosition, to, Color.red, 10.0f);
+        //    RaycastHit2D hit = Physics2D.Raycast(currentPosition, to, distance, 1 << LayerMask.NameToLayer("Obstacle"));
+
+        //    if (hit)
+        //    {
+        //        //we hit something:
+        //        print("HIT HIT HIT");
+        //        print(hit.transform.gameObject.name);
+        //        newVectorPath.Add(previousPosition);
+        //        currentPosition = previousPosition;
+
+        //    }
+        //    previousPosition = nextWaypoint;
+
+
+
+        //}
+
+        for(int i = 0; i < p.vectorPath.Count; i++)
+        {
+            Vector3 nextWaypoint = p.vectorPath[i];
+
+            float distance = Vector2.Distance(currentPosition, nextWaypoint);
+
+            Vector3 to = (nextWaypoint - currentPosition);
+
+            //Debug.DrawRay(currentPosition, to, Color.red, 10.0f);
+            RaycastHit2D hit = Physics2D.Raycast(currentPosition, to, distance, 1 << LayerMask.NameToLayer("Obstacle"));
 
             if (hit)
             {
@@ -142,17 +171,18 @@ public class EnemyAI : MonoBehaviour
                 print(hit.transform.gameObject.name);
                 newVectorPath.Add(previousPosition);
                 currentPosition = previousPosition;
+                i -= 1;
 
             }
             previousPosition = nextWaypoint;
 
-
-
         }
+
+
         // add the target at the end.
         newVectorPath.Add(target.position);
 
-        print(newVectorPath.Count);
+        
 
         return newVectorPath;
     }
@@ -183,7 +213,7 @@ public class EnemyAI : MonoBehaviour
 
         Vector2 force = direction * speed * Time.deltaTime;
 
-        //rb.AddForce(force);
+        rb.AddForce(force);
 
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
