916634e8e8e0f3d4f7ee5aa8667f6a822ec25836
KellyButic <49516066+KellyButic@users.noreply.github.com>
2021-04-17 20:14:45 +1000

    Version 3- with Radius

--- Assets/Scripts/OverlordMultiCast.cs
+++ Assets/Scripts/OverlordMultiCast.cs
@@ -8,18 +8,29 @@ public class OverlordMultiCast : MonoBehaviour
     public Transform frontLeft;
     public Transform frontRight;
     public Transform frontCenter;
+
+    public Transform leftUpper;
+    public Transform rightUpper;
+
     public LayerMask obstacleLayerMask;
+
     public float rayLength = .9f;
+    public float sideRadius = .6f;
+
     public bool isHittingObstacle = false;
+
     bool leftStatus = false;
     bool rightStatus = false;
     bool centerStatus = false;
+    bool leftUpperStatus = false;
+    bool rightUpperStatus = false;
+
     //collect the outcomes of this raycast hits
     public bool[] hitOutcomes;
 
     public void Awake()
     {
-        hitOutcomes = new bool[3];
+        hitOutcomes = new bool[5];
     }
 
 
@@ -34,6 +45,9 @@ public class OverlordMultiCast : MonoBehaviour
         Vector2 leftStartPoint = frontLeft.position;
         Vector2 rightStartPoint = frontRight.position;
         Vector2 centerStartPoint = frontCenter.position;
+
+        Collider2D leftCollider = Physics2D.OverlapCircle(leftUpper.position, sideRadius, obstacleLayerMask);
+        Collider2D rightCollider = Physics2D.OverlapCircle(rightUpper.position, sideRadius, obstacleLayerMask);
 
         RaycastHit2D frontLeftHit = Physics2D.Raycast(leftStartPoint, Vector2.up, rayLength, obstacleLayerMask);
         RaycastHit2D frontRightHit = Physics2D.Raycast(rightStartPoint, Vector2.up, rayLength, obstacleLayerMask);
@@ -72,16 +86,39 @@ public class OverlordMultiCast : MonoBehaviour
         {
             rightStatus = false;
         }
+        // UPPER LEFT
+        if (leftCollider != null)
+        {
+            leftUpperStatus = true;
+            Debug.Log("UPPER LEFT HIT:: " + leftUpperStatus);
+        }
+        else
+        {
+            leftUpperStatus = false;
+            Debug.Log("UPPER RIGHT HIT:: " + leftUpperStatus);
+        }
+        // UPPER RIGHT
+        if (rightCollider != null)
+        {
+            rightUpperStatus = true;
+            Debug.Log("UPPER RIGHT HIT:: " + rightUpperStatus);
+        }
+        else
+        {
+            rightUpperStatus = false;
+            Debug.Log("UPPER RIGHT HIT:: " + rightUpperStatus);
+        }
 
         hitOutcomes[0] = leftStatus;
         hitOutcomes[1] = rightStatus;
         hitOutcomes[2] = centerStatus;
+        hitOutcomes[3] = leftUpperStatus;
+        hitOutcomes[4] = rightUpperStatus;
 
-        for (int i= 0; i < 3; i++)
+        for (int i = 0; i < 5; i++)
         {
             Debug.Log("Hit on Element[" + i + "]:" + hitOutcomes[i]);
         }
-
 
     }
 
@@ -91,6 +128,9 @@ public class OverlordMultiCast : MonoBehaviour
         Gizmos.DrawLine(frontLeft.position, new Vector2(frontLeft.position.x, frontLeft.position.y + rayLength));
         Gizmos.DrawLine(frontRight.position, new Vector2(frontRight.position.x, frontRight.position.y + rayLength));
         Gizmos.DrawLine(frontCenter.position, new Vector2(frontCenter.position.x, frontCenter.position.y + rayLength));
+
+        Gizmos.DrawWireSphere(leftUpper.position, sideRadius);
+        Gizmos.DrawWireSphere(rightUpper.position, sideRadius);
     }
 
 
