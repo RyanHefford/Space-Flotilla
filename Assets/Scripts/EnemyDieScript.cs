3e34516abfeee330eb2dc45ebdf9306f642d821f
s3721994 <s3721994@student.rmit.edu.au>
2021-04-18 16:10:40 +1000

    Fixed minor merge issues

---
+++ Assets/Scripts/EnemyDieScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDieScript : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject pickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Die()
+    {
+        GameObject startExplosion = Instantiate<GameObject>(explosion);
+
+        if (Random.Range(0, 100) < 1)
+        {
+            Instantiate<GameObject>(pickup).transform.SetPositionAndRotation(transform.position, transform.rotation);
+        }
+
+        startExplosion.transform.SetPositionAndRotation(this.transform.position, this.transform.rotation);
+        startExplosion.transform.localScale = new Vector3(.5f, .5f, .5f);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Pathfinding/Hit.cs
+++ Assets/Scripts/Pathfinding/Hit.cs
@@ -6,7 +6,7 @@ public class Hit : MonoBehaviour
 {
     //When a flock hits a player.
     private Flock flock;
-
+    private GameObject player;
     private Score score;
 
     private OverlordManager om;
@@ -16,6 +16,8 @@ public class Hit : MonoBehaviour
     {
         //getting the OverlordManager script from the Manager GameObject
         om = GameObject.Find("Manager").GetComponent<OverlordManager>();
+
+        player = GameObject.Find("Player");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,7 +28,70 @@ public class Hit : MonoBehaviour
             flock = GameObject.Find("Flock").GetComponent<Flock>();
             FlockAgent agentToDelete = collision.gameObject.GetComponent<FlockAgent>();
             flock.agents.Remove(agentToDelete);
-            Destroy(collision.gameObject);
+            collision.gameObject.GetComponent<EnemyDieScript>().Die();
+
+            //update the score when we are shooting with the missle:
+            if (this.gameObject.tag == "Missle")
+            {
+                score = GameObject.Find("Canvas").GetComponent<Canvas>().GetComponent<Score>();
+                score.updateScore(10);
+            }
+
+            if (this.gameObject.tag == "Player")
+            {
+                player.GetComponent<Health>().takeDamage(1.0f);
+            }
+
+        }
+
+        //If we hit an overlord with the missle.
+        if (collision.gameObject.tag == "Overlord")
+        {
+            flock = GameObject.Find("Flock").GetComponent<Flock>();
+            //reset the agents that are sticking to the overlord
+            //and make them not STICk to the overlord
+            resetStickToOverlordAgents();
+            //remove the Overlord.
+            FlockAgent agentToDelete = collision.gameObject.GetComponent<FlockAgent>();
+            flock.agents.Remove(agentToDelete);
+            //overlord does not exist
+            om.overlordExists = false;
+
+            //more points for destroying an overlord
+            //update the score when we are shooting with the missle:
+            if (this.gameObject.tag == "Missle")
+            {
+                score = GameObject.Find("Canvas").GetComponent<Canvas>().GetComponent<Score>();
+                score.updateScore(30);
+            }
+
+            if (this.gameObject.tag == "Player")
+            {
+                player.GetComponent<Health>().takeDamage(1.0f);
+            }
+
+            print("HIT");
+            collision.gameObject.GetComponent<EnemyDieScript>().Die();
+        }
+
+        //destroying the missle. If statement because the player has the same
+        //script attached to it.
+        if (this.gameObject.tag == "Missle")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //remove from the list.
+        if (collision.gameObject.tag == "Agent")
+        {
+            flock = GameObject.Find("Flock").GetComponent<Flock>();
+            FlockAgent agentToDelete = collision.gameObject.GetComponent<FlockAgent>();
+            flock.agents.Remove(agentToDelete);
+            collision.gameObject.GetComponent<EnemyDieScript>().Die();
 
             //update the score when we are shooting with the missle:
             if (this.gameObject.tag == "Missle")
@@ -58,16 +123,8 @@ public class Hit : MonoBehaviour
                 score.updateScore(30);
             }
             print("HIT");
-            Destroy(collision.gameObject);
+            collision.gameObject.GetComponent<EnemyDieScript>().Die();
         }
-
-        //destroying the missle. If statement because the player has the same
-        //script attached to it.
-        if (this.gameObject.tag == "Missle")
-        {
-            Destroy(this.gameObject);
-        }
-
     }
 
     private void resetStickToOverlordAgents()
