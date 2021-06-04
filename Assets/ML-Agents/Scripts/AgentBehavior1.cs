f18924ecf45f9755b904b353d23bc49fb2c05bd3
Talal Meshari <talalmeshari@gmail.com>
2021-06-05 03:18:41 +1000

    Half of the new scene is done

---
+++ Assets/ML-Agents/Scripts/AgentBehavior1.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class AgentBehavior1 : Agent
+{
+
+    private Flock flock;
+    private PlayerMovement pm;
+    private PlayerShoot ps;
+    public SpriteRenderer sr;
+    private Health health;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        flock = transform.parent.Find("Flock").GetComponent<Flock>();
+        pm = GetComponent<PlayerMovement>();
+        ps = GetComponent<PlayerShoot>();
+        health = GetComponent<Health>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        if(flock.agents.Count == 0)
+        {
+            win();
+            EndEpisode();
+        }
+
+
+    }
+
+    public int getTotalCount()
+    {
+        return Academy.Instance.TotalStepCount;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+
+        //reset player position
+        transform.localPosition = new Vector3(0, -6.5f, 0);
+        //reset the flocks
+        destroyAgents();
+        flock.startNewEp();
+
+        //reset player health
+        health.playerHealth = 10;
+
+        //reset player rotation
+        transform.rotation = new Quaternion(0,0, 0, 0);
+
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+        //Things the agent need to know in the world.
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(transform.localRotation);
+        sensor.AddObservation(flock.agents.Count);
+        sensor.AddObservation(health.playerHealth);
+        
+        //50 steps = 1 second
+        sensor.AddObservation((float)StepCount / MaxStep);
+        //shoot
+        sensor.AddObservation(ps.shoot);
+        
+
+    }
+
+
+    //where the actions of the player happens.
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+
+        float movementSpeed = 4.0f;
+        float rotationSpeed = 5.0f;
+        float rotationAngle = 10.0f;
+        int movement = actions.DiscreteActions[0];
+        int rotation = actions.DiscreteActions[1];
+        int shoot = actions.DiscreteActions[2];
+
+        Vector2 movementForceDirection = movementVector(movement);
+
+        //add the movement force
+        //Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        //rb.AddForce(movementForceDirection * movementSpeed);
+
+
+
+        transform.localPosition += (Vector3)movementForceDirection * movementSpeed * Time.deltaTime;
+
+        //positive = <-
+        //negative = ->
+        switch (rotation)
+        {
+            case 0:
+                //no rotation
+                //transform.localRotation.Set(0, 0, 0, rotationAngle * Time.deltaTime * rotationSpeed);
+                transform.localEulerAngles += new Vector3(0, 0, 0);
+                //transform.Rotate(new Vector3(0, 0, 0), rotationAngle * Time.deltaTime * rotationSpeed);
+                break;
+            case 1:
+                //rotate right
+                //transform.localRotation.Set(0, 0, -1, rotationAngle * Time.deltaTime * rotationSpeed);
+                //transform.localRotation = Quaternion.Euler(0, 0, -1 + rotationAngle * Time.deltaTime * rotationSpeed);
+                transform.localEulerAngles += new Vector3(0, 0, -1) * rotationAngle * Time.deltaTime * rotationSpeed;
+                //transform.Rotate(new Vector3(0, 0, -1), rotationAngle * Time.deltaTime * rotationSpeed);
+                break;
+            case 2:
+                //rotate left
+                //transform.localRotation.Set(0, 0, 1, rotationAngle * Time.deltaTime * rotationSpeed);
+                //transform.localRotation = Quaternion.Euler(0, 0, 1 + rotationAngle * Time.deltaTime * rotationSpeed);
+                transform.localEulerAngles += new Vector3(0, 0, 1) * rotationAngle * Time.deltaTime * rotationSpeed;
+                //transform.Rotate(new Vector3(0, 0, 1), rotationAngle * Time.deltaTime * rotationSpeed);
+                break;
+            default:
+                //no rotation
+                //transform.localRotation.Set(0, 0, 0, rotationAngle * Time.deltaTime * rotationSpeed);
+                //transform.localRotation = Quaternion.Euler(0, 0, 0);
+                transform.localEulerAngles += new Vector3(0, 0, 0);
+                //transform.Rotate(new Vector3(0, 0, 0), rotationAngle * Time.deltaTime * rotationSpeed);
+                break;
+        }
+
+        ps.shoot = shoot;
+
+    }
+
+
+    private void destroyAgents()
+    {
+
+        foreach(Transform child in flock.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        //clear the list.
+        flock.agents.Clear();
+
+    }
+
+    public void lose()
+    {
+        sr.color = Color.red;
+    }
+    public void win()
+    {
+        sr.color = Color.green;
+    }
+
+
+    private Vector2 movementVector(int movement)
+    {
+        Vector2 forceDirection = Vector2.zero;
+
+        switch (movement)
+        {
+            case 0:
+                //no movement
+                forceDirection = Vector2.zero;
+                break;
+            case 1:
+                //Move right
+                forceDirection = new Vector2(1.0f, 0.0f);
+                break;
+            case 2:
+                //Move left
+                forceDirection = new Vector2(-1.0f, 0.0f);
+                break;
+            case 3:
+                //Move up
+                forceDirection = new Vector2(0.0f, 1.0f);
+                break;
+            case 4:
+                //Move down
+                forceDirection = new Vector2(0.0f, -1.0f);
+                break;
+            case 5:
+                //Move top-right
+                forceDirection = new Vector2(1.0f, 1.0f);
+                break;
+            case 6:
+                //Move top-left
+                forceDirection = new Vector2(-1.0f, 1.0f);
+                break;
+            case 7:
+                //Move bottom-right
+                forceDirection = new Vector2(1.0f, -1.0f);
+                break;
+            case 8:
+                //Move left
+                forceDirection = new Vector2(-1.0f, -1.0f);
+                break;
+            default:
+                //Don't move
+                forceDirection = Vector2.zero;
+                break;
+        }
+        return forceDirection;
+    }
+
+
+}
+
+
---
+++ Assets/Scripts/Pathfinding/Hit2.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class Hit2 : MonoBehaviour
+{
+    //When a flock hits a player.
+    private Flock flock;
+
+    public Health health;
+    private Score score;
+    private AgentBehavior2 ab;
+
+    private void Start()
+    {
+        //getting the OverlordManager script from the Manager GameObject
+       // om = GameObject.Find("Manager").GetComponent<OverlordManager>();
+        flock =  getFlock();
+        ab = getAgentBehavior();
+    }
+
+    private Flock getFlock()
+    {
+        Flock newFlock = null;
+        if(this.transform.gameObject.tag == "Player")
+        {
+            newFlock =  this.transform.parent.Find("Flock").GetComponent<Flock>();
+        }
+        else if(this.transform.gameObject.tag == "Missle" || this.transform.gameObject.tag == "HyperBeam")
+        {
+            newFlock = this.transform.parent.transform.parent.Find("Flock").GetComponent<Flock>();
+        }
+
+        return newFlock;
+    }
+    private AgentBehavior2 getAgentBehavior()
+    {
+        AgentBehavior2 ab = null;
+        if (this.transform.gameObject.tag == "Player")
+        {
+            ab = GetComponent<AgentBehavior2>();
+        }
+        else if (this.transform.gameObject.tag == "Missle" || this.transform.gameObject.tag == "HyperBeam")
+        {
+            ab = this.transform.parent.GetComponent<AgentBehavior2>();
+        }
+
+        return ab;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        flock = getFlock();
+
+       
+
+        //destroying the missle. If statement because the player has the same
+        //script attached to it.
+        if (this.gameObject.tag == "Missle" && collision.gameObject.tag == "Wall")
+        {
+
+            float penalty = -0.3f;
+
+            Destroy(this.gameObject);
+            
+            float scale_factor = Mathf.Min(1, ab.StepCount / 200000);
+
+            float scaled_penalty = penalty * scale_factor;
+            //Debug.Log(ab.getTotalCount());
+            //Debug.Log(scaled_penalty);
+
+            //ab.AddReward(scaled_penalty);
+
+            //NOT USED
+
+        }
+
+        //remove from the list.
+        if (collision.gameObject.tag == "Agent")
+        {
+
+            FlockAgent agentToDelete = collision.gameObject.GetComponent<FlockAgent>();
+
+            flock.agents.Remove(agentToDelete);
+            //collision.gameObject.GetComponent<EnemyDieScript>().Die();
+            Destroy(agentToDelete.gameObject);
+
+            //update the score when we are shooting with the missle:
+            if (this.gameObject.tag == "Missle")
+            {
+                //score = GameObject.Find("Canvas").GetComponent<Canvas>().GetComponent<Score>();
+                //score.updateScore(10);
+
+                //add reward for the agent
+                ab.AddReward(1.0f);
+            }
+
+            if (this.gameObject.tag == "Player")
+            {
+                ab.AddReward(-0.7f);
+                health.takeDamage(1.0f);
+                //ab.lose();
+                //ab.EndEpisode();
+            }
+
+        }
+
+        if (this.gameObject.tag == "Player" && collision.gameObject.tag == "Wall")
+        {
+            //ab.AddReward(-0.1f);
+            ab.AddReward(-0.3f);
+            //ab.lose();
+            //ab.EndEpisode();
+        }
+
+
+        if (this.gameObject.tag == "Missle")
+        {
+            //in case two missles collide.
+            Destroy(this.gameObject);
+        }
+    }
+
+
+
+}
